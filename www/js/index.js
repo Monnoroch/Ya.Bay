2c6433116476554051a164be928b82fa50860a39
tolya <derujov@gmail.com>
2014-10-11 21:34:21 +0400

    new data render

--- www/js/index.js
+++ www/js/index.js
@@ -24,10 +24,26 @@ Number.prototype.printInt = function (){
   return this.printFloat(0);
 }
 
+var processHash = function( url ) {
+    var parsed = $.mobile.path.parseUrl( url ),
+        hashQuery = parsed.hash.split( "?" );
+    return {
+        parsed: parsed,
+        cleanHash: ( hashQuery.length > 0 ? hashQuery[ 0 ] : "" ),
+        queryParameters: ( hashQuery.length > 1 ? hashQuery[ 1 ] : "" )
+    };
+};
+
 var app = {
     // Application Constructor
     initialize: function() {
+        var self = this;
+        
         this.bindEvents();
+        
+        //this.fetchCats(function(){
+            //self.renderCats(self.cats);
+        //});
     },
     // Bind Event Listeners
     //
@@ -63,55 +79,147 @@ var app = {
             $.mobile.allowCrossDomainPages = true;
             $.support.cors = true;
             $.mobile.ignoreContentEnabled = true;
-            $.mobile.defaultPageTransition = "slide";
         });
-
-        var filter = [];
-        var categ_data;
-
-        $(".b-popcat-table__item_type_phone").click(function(){
-            $.mobile.changePage("#item_type_phone_menu");
-            categ_data = data.filter(function(val){ return val.categ === "phones"});
-            fillList(categ_data, $("#item_type_phone_menu > .ui-content > .b-serp"));
-        });
-        $(".b-popcat-table__item_type_tv").click(function(){
-            $.mobile.changePage("#item_type_tv_menu");
-            categ_data = data.filter(function(val){ return val.categ === "tvs"});
-            fillList(categ_data, $("#item_type_tv_menu > .ui-content > .b-serp"));
-        });
-
-        function os_click_handler(os) {
-            return function(e) {
-                $("#item_type_phone_menu > .ui-content > .b-serp").empty();
-                $(this).toggleClass("activated");
+         $.mobile.defaultPageTransition = 'none';
+         $.mobile.defaultDialogTransition = 'none';
+        
+        $(document)
+        .on( "pagechange", function( event, data ) {
+            
+        })
+        .on( "pagebeforechange", function( event, data ) {
+            // When we go from #secondary-page to #secondary-page we wish to indicate
+            // that a transition to the same page is allowed.
+            
+            
+            if ( $.type( data.toPage ) === "string" &&
+                data.options.fromPage &&
+                data.options.fromPage.attr( "id" ) === "main_menu" &&
+                processHash( data.toPage ).cleanHash === "#main_menu" ) {
+             
+                    var queryParameters = {},
+                        processedHash = processHash( data.toPage );
+                        
+                    if ( processedHash.queryParameters ) {
+                        $.each( processedHash.queryParameters.split( "&" ),
+                            function( index, value ) {
+                                var pair = value.split( "=" );
+                                if ( pair.length > 0 && pair[ 0 ] ) {
+                                    queryParameters[ pair[ 0 ] ] =
+                                        ( pair.length > 1 ? pair[ 1 ] : true );
+                                }
+                            });
+                    }
+                    if( self.path.join(",") != queryParameters.cat ){
+                    //data.options.openmenu = true;
+                        data.options.allowSamePageTransition = true;
+                    }
+                }
+        })
+        .on( "pagecontainerbeforetransition", function( event, data ) {
+            var queryParameters = {},
+                processedHash = processHash( data.absUrl );
+            // We only modify default behaviour when navigating to the secondary page
+            if ( processedHash.cleanHash === "#main_menu" ) {
+                // Assemble query parameters object from the query string
+                if ( processedHash.queryParameters ) {
+                    $.each( processedHash.queryParameters.split( "&" ),
+                        function( index, value ) {
+                            var pair = value.split( "=" );
+                            if ( pair.length > 0 && pair[ 0 ] ) {
+                                queryParameters[ pair[ 0 ] ] =
+                                    ( pair.length > 1 ? pair[ 1 ] : true );
+                            }
+                        });
+                }
+             
+             
+                var cat = self.cats;
+                var parent = [];
+                var path = self.path = [];
+                var check = self.check = false;
                 
-                if($(this).hasClass('activated')) {
-                    filter.push(os);
-                } else {
-                    filter.splice( filter.indexOf(os), 1 );
+                if( queryParameters.cat ){
+                    path = self.path = queryParameters.cat.split(",");
+                    parent = path.slice(0,-1);
+                    
+                    
+                    for(var i = 0; i < path.length; i++){
+                        cat = cat[ path[i] ].children;
+                        if( !cat ){
+                            cat = self.cats;
+                            break;
+                        }
+                    }    
+                }
+                if( queryParameters.check !== undefined ){
+                    if( queryParameters.check == "" ){
+                        check = self.check = [];
+                    }else{
+                        check = self.check = queryParameters.check.split(",");
+                    }
+                }
+                
+                if(cat){
+                    self.renderCats( cat, path.join(",") );
+                    self.renderItems();
                 }
 
-                console.log(filter);
-
-                var filtered_data
-                if(filter.length == 0)
-                    filtered_data = categ_data;
-                else {
-                        filtered_data = categ_data.filter(function(val) {
-                        console.log(val.os)
-                        console.log(filter.indexOf(val.os))
-                        return filter.indexOf(val.os) !== -1;
-                    });
-                }
-
-                fillList(filtered_data, $("#item_type_phone_menu > .ui-content > .b-serp"));
+                // Set the title from the query parameters
+                //$( "#section" ).text( queryParameters.section );
+                // Set the url of the page - this will be used by navigation to set the
+                // URL in the location bar
+                $( "#main_menu" ).jqmData( "url", processedHash.parsed.hash );
             }
-        }
+        });
+        
 
-        $(".b-popcat-table__item_type_android").click(os_click_handler("android"));
-        $(".b-popcat-table__item_type_ios").click(os_click_handler("ios"));
-        $(".b-popcat-table__item_type_blackberry").click(os_click_handler("blackberry")); 
-        $(".b-popcat-table__item_type_windows").click(os_click_handler("windows")); 
+//         var filter = [];
+//         var categ_data;
+// 
+//         $(".b-popcat-table__item_type_phone").click(function(){
+//             $.mobile.changePage("#item_type_phone_menu");
+//             categ_data = data.filter(function(val){ return val.categ === "phones"});
+//             fillList(categ_data, $("#item_type_phone_menu > .ui-content > .b-serp"));
+//         });
+//         $(".b-popcat-table__item_type_tv").click(function(){
+//             $.mobile.changePage("#item_type_tv_menu");
+//             categ_data = data.filter(function(val){ return val.categ === "tvs"});
+//             fillList(categ_data, $("#item_type_tv_menu > .ui-content > .b-serp"));
+//         });
+
+//         function os_click_handler(os) {
+//             return function(e) {
+//                 $("#item_type_phone_menu > .ui-content > .b-serp").empty();
+//                 $(this).toggleClass("activated");
+//                 
+//                 if($(this).hasClass('activated')) {
+//                     filter.push(os);
+//                 } else {
+//                     filter.splice( filter.indexOf(os), 1 );
+//                 }
+// 
+//                 console.log(filter);
+// 
+//                 var filtered_data
+//                 if(filter.length == 0)
+//                     filtered_data = categ_data;
+//                 else {
+//                         filtered_data = categ_data.filter(function(val) {
+//                         console.log(val.os)
+//                         console.log(filter.indexOf(val.os))
+//                         return filter.indexOf(val.os) !== -1;
+//                     });
+//                 }
+// 
+//                 fillList(filtered_data, $("#item_type_phone_menu > .ui-content > .b-serp"));
+//             }
+//         }
+// 
+//         $(".b-popcat-table__item_type_android").click(os_click_handler("android"));
+//         $(".b-popcat-table__item_type_ios").click(os_click_handler("ios"));
+//         $(".b-popcat-table__item_type_blackberry").click(os_click_handler("blackberry")); 
+//         $(".b-popcat-table__item_type_windows").click(os_click_handler("windows")); 
     },
     // deviceready Event Handler
     //
@@ -120,20 +228,155 @@ var app = {
     onDeviceReady: function() {
         var self = this;
 
-        setTimeout(function() {
-            $.mobile.changePage("#main_menu");
-            fillList(data, $("#main_menu > .ui-content > .b-serp"));
-        }, 100);
+        //setTimeout(function() {
+            //$.mobile.changePage("#main_menu");
+            //fillList(data, $("#main_menu > .ui-content > .b-serp"));
+        //}, 100);
         
         $( "#swipe-region" ).on( "swiperight", function(e) {
             e.stopImmediatePropagation();
             $( "#menu" ).panel( "open" );
         } );
+        
+        
+        this.fetchCats(function(){
+            self.fetchItems(function(){
+                $.mobile.changePage("#main_menu");
+            });
+            //self.renderCats(self.cats);
+        });
+        
+        
 
         //this.onOrientationChange();
     },
     onOrientationChange: function() {
 
-    }
+    },
+    
+    fetchCats: function(callback){
+        var self = this;
+       $.ajax({
+            url: "data/cats.json",
+            success: function(data){
+                self.cats = data;
+                callback();
+            },
+            dataType: "json"
+        });
+    },
+    
+    fetchItems: function(callback){
+        var self = this;
+       $.ajax({
+            url: "data/items.json",
+            success: function(data){
+                self.items = data;
+                callback();
+            },
+            dataType: "json"
+        });
+    },
+    
+    
+    
+    renderCats: function(cat, parent){
+        var cont = $("#cats").empty();
+        var href;
+        var check;
+        var checkIndex;
+        var newCheck;
+        
+        if( !this.check ){
+            this.check = [];
+            for(var i = 0; i < cat.length; i++){
+                if( !cat[i].children && cat[i].check ){
+                    this.check.push( "" + i);
+                }
+            }
+        }
+        
+        for(var i = 0; i < cat.length; i++){
+            if( cat[i].children ){
+                href = "#main_menu?cat=" + (parent?(parent+","):"") + i
+                check = false;
+            }
+            else{
+                    checkIndex = this.check.indexOf( i.toString() );
+                    if( checkIndex >= 0 /*|| cat[i].check*/ ){
+                        check = true;
+                        newCheck = [].concat(this.check)
+                        newCheck.splice( checkIndex , 1);
+                    }else{
+                        check = false;
+                        newCheck = [i].concat(this.check);
+                    }
+                cat[i].check = check;
+                href = "#main_menu?cat=" + (parent?parent:"") + "&check=" + newCheck.join(",")  ;
+            }
+            cont.append(  $("<div>", {class: "cats-item " + (check?"check":"")})
+                .append( $("<a>", {href: href })
+                    .append( $("<i>", {class: "icon " + cat[i].icon}) )
+                    .append( $("<span>").append(cat[i].name) )
+                )
+            )
+        }   
+    },
+    
+    renderItems: function(){
+        var cont = $("#items").empty();
+        
+        var items = this.filterItems();
+        
+        for(var  i = 0; i < items.length; i++){
+            var data = items[i];
+            cont.append( $("<div>", {class:"b-serp-item"})
+            
+                .append ( $( "<a>", { class:"b-link",  href: "#" + data.id  } )
+                    .append( $( "<i>", { class: "b-icon", style: "background-image: url(" + data.img + ")" } ) )
+                    .append( $( "<div>", { class: "b-serp-description"} )
+                        .append( $("<div>", { class: "b-serp-description__title" }) .append( data.title ) )
+                        .append( $("<div>", { class: "b-serp-description__text b-serp-description__text_limit_yes"} ).append( data.text ) )
+                    )
+                )
+            )
+            
+            //cont.append( $("<div>").append( items[i].title ) )
+        }
+    },
+    
+    
+    filterItems: function(){
+        var path = this.path;
+        var result = [];
+        var f = false;
+        
+        if( path && path.length > 0 ){
+            for( var i = 0; i < this.items.length; i++ ){
+                if( this.items[i].cat.length >= path.length ){
+                    f = true;
+                    for( var j = 0; j < path.length; j++ ){
+                        if( path[j] != this.items[i].cat[j] ){
+                            f = false;
+                            break;
+                        }
+                    }
+                    if( f && this.check.length > 0 ){
+                        if( this.check.indexOf( this.items[i].cat[ path.length ].toString() ) < 0 ){
+                            f = false;
+                        }
+                    }
+                    if(f){
+                        result.push( this.items[i] );
+                    }
+                }
+            }
+        }
+        else{
+            result = this.items;
+        }
+        return result;
+    },
+    
 
 };
