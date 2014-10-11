bce460e661a3bc0fb71c796314df378bb7253675
monnoroch <monnoroch@gmail.com>
2014-10-11 13:27:48 +0400

    temporary removed landing

--- www/js/index.js
+++ www/js/index.js
@@ -72,7 +72,7 @@ var app = {
         setTimeout(function() {
             $("#landing").hide();
             $("#mapapp").show();
-        }, 5000);
+        }, 0);
 
         //this.onOrientationChange();
     },
